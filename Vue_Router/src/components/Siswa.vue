cc1ea62d7d1fc629c6bd469e4d55241f8a995d17
farhan259 <47270330+farhan259@users.noreply.github.com>
2019-03-18 16:40:30 +0700

    Add files via upload

---
+++ Vue_Router/src/components/Siswa.vue
@@ -0,0 +1,29 @@
+<template>
+ <div><br>
+  <center>
+  <table border="1">
+    <tr>
+      <td><center><b>Kelas</b></center></td>
+      <td><center><b>Nama</b></center></td>
+    </tr>
+    <tr>
+      <td>XII - RPL 1</td>
+      <td>Farhan Fauzan</td>
+    </tr>
+    <tr>
+      <td>XII - TITL 2</td>
+      <td>Hanny Suriah</td>
+    </tr>
+    <tr>
+      <td>XII - TITL 3</td>
+      <td>Rendy De Punyet</td>
+    </tr>
+  </table>
+  </center>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
